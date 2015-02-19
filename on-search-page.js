3ffef8c3cfb056dda35dd1e512761e0291d09a2a
Michael Maurer <mjmaurer7@gatech.edu>
2015-02-19 15:52:12 -0500

    Fixed status complete bug

--- on-search-page.js
+++ on-search-page.js
@@ -1,12 +1,17 @@
 //TODO sendMessage can send anything. Think about changing
 
+// TODO extra span of class ft appears
 $('span.st').html(function(index, old) {
-    return '<span class="querySeg">' + old.replace(/[\s\u00A0]\.\.\. ?/g, '</span> ... <span class="querySeg">') + '</span>';
+    return '<span class="querySeg">' + old.replace('...', '</span> ... <span class="querySeg">') + '</span>';
 });
 
+// /[\s\u00A0]\.\.\. ?/g
+
 $('span.querySeg').click(function(){
-    chrome.runtime.sendMessage({textToFind: 'hello'}, function(response) {});
-    window.location.href = $(this).closest('.g').find("a").attr("href");
+    var url = $(this).closest('.g').find("a").attr("href");
+    alert(url);
+    //chrome.runtime.sendMessage(url, function(response) {});
+    // window.location.href = $(this).closest('.g').find("a").attr("href");
     //alert(this.textContent);
     //alert($(this).closest('.g').find("a").attr("href"));
 });
