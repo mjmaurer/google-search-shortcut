e908383e5f920d2822fe7c96abd0645d1ec6c407
Michael Maurer <mjmaurer7@gatech.edu>
2015-02-19 02:23:40 -0500

    Segment pages search pages with new and beautiful Jquery

--- on-search-page.js
+++ on-search-page.js
@@ -1,29 +1,10 @@
 //TODO sendMessage can send anything. Think about changing
 
-$("em").click(function(){
-    var result = "";
-    var current = this.previousSibling;
-    while (current != null) {
-        sep = current.textContent.split(" ... ");
-        if (sep.length == 1) {
-            result = sep[0] + result;
-        } else {
-            result = sep[sep.length-1] + result;
-            break;
-        }
-        current = current.previousSibling;
-    }
-    result = result + this.textContent;
-    current = this.nextSibling;
-    while (current != null) {
-        sep = current.textContent.split(/[\s\u00A0]... ?/);
-        if (sep.length == 1) {
-            result = result + sep[0];
-        } else {
-            result = result + sep[0];
-            break;
-        }
-        current = current.nextSibling;
-    }
+$('span.st').html(function(index, old) {
+    return '<span class="querySeg">' + old.replace('...', '</span>...<span class="querySeg">') + '</span>';
+});
+
+$('span.querySeg').click(function(){
+    alert(this.textContent);
     alert($(this).closest('.g').find("a").attr("href"));
 });
