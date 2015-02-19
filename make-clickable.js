484023cb2bec8ec893b09fb73d139fd1045bce8a
Michael Maurer <mjmaurer7@gatech.edu>
2015-02-19 14:33:23 -0500

    Fixed the find and replace

--- make-clickable.js
+++
@@ -1,5 +0,0 @@
-function sendMessage(p1, p2) {
-    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {});
-}
-
-document.body.innerHTML = document.body.innerHTML.replace(/<em>/g, '<em onclick=send-word.js>');
--- on-search-page.js
+++ on-search-page.js
@@ -1,10 +1,12 @@
 //TODO sendMessage can send anything. Think about changing
 
 $('span.st').html(function(index, old) {
-    return '<span class="querySeg">' + old.replace('...', '</span>...<span class="querySeg">') + '</span>';
+    return '<span class="querySeg">' + old.replace(/[\s\u00A0]\.\.\. ?/g, '</span> ... <span class="querySeg">') + '</span>';
 });
 
 $('span.querySeg').click(function(){
-    alert(this.textContent);
-    alert($(this).closest('.g').find("a").attr("href"));
+    chrome.runtime.sendMessage({textToFind: 'hello'}, function(response) {});
+    window.location.href = $(this).closest('.g').find("a").attr("href");
+    //alert(this.textContent);
+    //alert($(this).closest('.g').find("a").attr("href"));
 });
