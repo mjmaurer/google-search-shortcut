3f3423b7169a95e5d478cb88230a715158677e0d
Michael Maurer <mjmaurer7@gatech.edu>
2015-02-18 22:48:04 -0500

    Not going to keep any of this. Probably should branch

--- on-search-page.js
+++ on-search-page.js
@@ -1,16 +1,38 @@
-// Sends message to the background script from the current tab
-function sendMessageFromTab(message) {
-    chrome.tabs.getCurrent(function(currentTab) {
-        if (typeof currentTab === 'undefined') { 
-            // currentTab is optional parameter
-            myVariable = 'default';
-        } else {
-            // Send message with text to find
-            // chrome.runtime.sendMessage({textToFind: message}, function(response) {});
-        }
-    });
+//TODO sendMessage can send anything. Think about changing
+
+var searchTerms = document.getElementsByTagName("em");
+
+var findAndSend = function(element) {
+    
+    // var result = "";
+    // var current = element.previousSibling;
+    // while (current != null) {
+    //     sep = current.textContent.split(" ... ");
+    //     if (sep.length == 1) {
+    //         result = sep[0] + result;
+    //     } else {
+    //         result = sep[sep.length-1] + result;
+    //         break;
+    //     }
+    //     current = current.previousSibling;
+    // }
+    // result = result + element.textContent;
+    // current = element.nextSibling;
+    // while (current != null) {
+    //     sep = current.textContent.split(/[\s\u00A0]... ?/);
+    //     if (sep.length == 1) {
+    //         result = result + sep[0];
+    //     } else {
+    //         result = result + sep[0];
+    //         break;
+    //     }
+    //     current = current.nextSibling;
+    // }
+    alert(result);
 }
 
-//TODO sendMessage can send anything. Think about changing
-document.body.innerHTML = document.body.innerHTML.replace(/<em>/g, '<em onclick=\"'
-        + 'chrome.runtime.sendMessage({textToFind: \'hello\'}, function(response) {});\">');
+
+// Change this to jquery
+for(i=0; i < searchTerms.length; i++) {
+    searchTerms.item(i).setAttribute("onclick", "(" + findAndSend.toString() + ")(this)");
+}
