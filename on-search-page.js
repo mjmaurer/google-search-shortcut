913739e78ed3ef9bc49830d6b7b0b08f273114ec
Michael Maurer <mjmaurer7@gatech.edu>
2015-02-19 23:22:09 -0500

    Fixed communication between pages

--- on-search-page.js
+++ on-search-page.js
@@ -2,15 +2,14 @@
 
 // TODO extra span of class ft appears
 $('span.st').html(function(index, old) {
-    return '<span class="querySeg">' + old.replace('...', '</span> ... <span class="querySeg">') + '</span>';
+    return '<span class="querySeg">' + old.replace(/(\s|&nbsp;|\.)+\.\.\.(\s|&nbsp;|\.)*/g, '</span> ... <span class="querySeg">') + '</span>';
 });
-
-// /[\s\u00A0]\.\.\. ?/g
 
 $('span.querySeg').click(function(){
     var url = $(this).closest('.g').find("a").attr("href");
-    alert(url);
-    //chrome.runtime.sendMessage(url, function(response) {});
+    chrome.runtime.sendMessage({newUrl:url, message:this.textContent}, function(response) {
+        window.location.href = url;
+    });
     // window.location.href = $(this).closest('.g').find("a").attr("href");
     //alert(this.textContent);
     //alert($(this).closest('.g').find("a").attr("href"));
